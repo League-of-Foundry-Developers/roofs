6b56cef2d88628f899c1c1532ada372812f61065
Vali <moerill@gmx.de>
2020-12-18 16:59:59 +0100

    Fix?

--- scripts/RoofsLayer.js
+++ scripts/RoofsLayer.js
@@ -1,4 +1,4 @@
-import { log, translatePoint, readPixel } from './helpers.js';
+import { log, translatePoint, readPixel } from "./helpers.js";
 
 export default class RoofsLayer extends CanvasLayer {
   constructor() {
@@ -7,9 +7,9 @@ export default class RoofsLayer extends CanvasLayer {
   }
 
   static init() {
-    log('Init RoofsLayer', true);
+    log("Init RoofsLayer", true);
     canvas.tiles.placeables.forEach((tile) => {
-      if (tile.getFlag('roofs', 'isRoof')) RoofsLayer.createRoof(tile);
+      if (tile.getFlag("roofs", "isRoof")) RoofsLayer.createRoof(tile);
     });
   }
 
@@ -19,7 +19,7 @@ export default class RoofsLayer extends CanvasLayer {
    */
   static async receiveTile(tile) {
     log(`Receiving tile ${tile.data._id}`);
-    await tile.setFlag('roofs', 'isRoof', true);
+    await tile.setFlag("roofs", "isRoof", true);
   }
 
   /**
@@ -28,7 +28,7 @@ export default class RoofsLayer extends CanvasLayer {
    */
   static async releaseTile(tile) {
     log(`Releasing tile ${tile.data._id}`);
-    await tile.setFlag('roofs', 'isRoof', false);
+    await tile.setFlag("roofs", "isRoof", false);
   }
 
   /**
@@ -36,15 +36,18 @@ export default class RoofsLayer extends CanvasLayer {
    * @param {Tile} tile  instance of Tile
    */
   static async toggleMode(tile) {
-    let mode = tile.getFlag('roofs', 'mode');
+    let mode = tile.getFlag("roofs", "mode");
     switch (mode) {
-      case 'auto': mode = 'open';
+      case "auto":
+        mode = "open";
         break;
-      case 'open': mode = 'closed';
+      case "open":
+        mode = "closed";
         break;
-      default: mode = 'auto';
+      default:
+        mode = "auto";
     }
-    await tile.setFlag('roofs', 'mode', mode);
+    await tile.setFlag("roofs", "mode", mode);
   }
 
   /**
@@ -94,7 +97,7 @@ export default class RoofsLayer extends CanvasLayer {
    */
   static _onUpdateTile(scene, data) {
     const tile = canvas.tiles.get(data._id);
-    const isRoof = tile.getFlag('roofs', 'isRoof');
+    const isRoof = tile.getFlag("roofs", "isRoof");
     // If isnt roof but is becoming one, createRoof
     if (isRoof && !tile.roof) RoofsLayer.createRoof(tile);
     // If was roof, but isn't now, destroyRoof
@@ -109,8 +112,8 @@ export default class RoofsLayer extends CanvasLayer {
    * @param {Tile} tile  Tile entity
    */
   static _onHoverTile(tile, state) {
-    if (!game.settings.get('roofs', 'quickPeek')) return;
-    if (!tile.getFlag('roofs', 'isRoof')) return;
+    if (!game.settings.get("roofs", "quickPeek")) return;
+    if (!tile.getFlag("roofs", "isRoof")) return;
     const { open } = RoofsLayer.getOpacity(tile);
     if (state) tile.roof.container.alpha = open;
     else RoofsLayer.setAlphas();
@@ -123,8 +126,8 @@ export default class RoofsLayer extends CanvasLayer {
    */
   static inBounds(tile, token) {
     // Get canvas coords of token, accounting for token center
-    const x = token.data.x + (token.data.width * canvas.grid.size / 2);
-    const y = token.data.y + (token.data.height * canvas.grid.size / 2);
+    const x = token.data.x + (token.data.width * canvas.grid.size) / 2;
+    const y = token.data.y + (token.data.height * canvas.grid.size) / 2;
     // Get local pos relative to tile
     const local = translatePoint({ x, y }, canvas.stage, tile.roof.sprite);
     // Adjust local pos to account for centered anchor
@@ -133,10 +136,10 @@ export default class RoofsLayer extends CanvasLayer {
     // Check if in bounds of the tile
     log(`Converted global (${x},${y}) to local (${local.x},${local.y})`);
     if (
-      local.x > 0
-      && local.y > 0
-      && local.x < tile.roof.sprite.width
-      && local.y < tile.roof.sprite.height
+      local.x > 0 &&
+      local.y > 0 &&
+      local.x < tile.roof.sprite.width &&
+      local.y < tile.roof.sprite.height
     ) {
       // Get alpha value of texture at coordinate
       const pixel = readPixel(tile.roof.wrapper, local.x, local.y);
@@ -160,9 +163,13 @@ export default class RoofsLayer extends CanvasLayer {
    * @param {Tile} tile  Foundry Tile entity
    */
   static getOpacity(tile) {
-    const open = tile.getFlag('roofs', 'open') ?? game.settings.get('roofs', 'defaultOpen');
-    const closed = tile.getFlag('roofs', 'closed') ?? game.settings.get('roofs', 'defaultClosed');
-    const mode = tile.getFlag('roofs', 'mode') ?? 'auto';
+    const open =
+      tile.getFlag("roofs", "open") ??
+      game.settings.get("roofs", "defaultOpen");
+    const closed =
+      tile.getFlag("roofs", "closed") ??
+      game.settings.get("roofs", "defaultClosed");
+    const mode = tile.getFlag("roofs", "mode") ?? "auto";
     return { open, closed, mode };
   }
 
@@ -177,13 +184,13 @@ export default class RoofsLayer extends CanvasLayer {
       const { container } = tile.roof;
       const { open, closed, mode } = RoofsLayer.getOpacity(tile);
       // If tile is in closed mode, just make sure it's closed
-      if (mode === 'closed') {
+      if (mode === "closed") {
         tile.roof.state = true;
         container.alpha = closed;
         return;
       }
       // If tile is in open mode, just make sure it's open
-      if (mode === 'open') {
+      if (mode === "open") {
         tile.roof.state = false;
         container.alpha = open;
         return;
@@ -199,19 +206,17 @@ export default class RoofsLayer extends CanvasLayer {
         tile.roof.state = false;
         if (game.user.isGM) container.alpha = open;
         else container.visible = false;
-      }
-      else if (inBounds) {
+      } else if (inBounds) {
         tile.roof.state = false;
         container.alpha = open;
-      }
-      else {
+      } else {
         tile.roof.state = true;
         container.alpha = closed;
       }
     });
     const t1 = performance.now();
     log(`setAlphas took ${t1 - t0}ms.`);
-    canvas.sight.update();
+    canvas.sight.refresh();
   }
 
   /**
@@ -219,11 +224,14 @@ export default class RoofsLayer extends CanvasLayer {
    */
   static _sightUpdate() {
     const t0 = performance.now();
-    if (!game.settings.get('roofs', 'autoHide')) return;
+    if (!game.settings.get("roofs", "autoHide")) return;
     canvas.tiles.placeables.forEach((tile) => {
       if (!tile.roof || !tile.roof.state || tile.hidden) return;
       canvas.tokens.placeables
-        .filter((token) => !token.observer && token.visible && RoofsLayer.inBounds(tile, token))
+        .filter(
+          (token) =>
+            !token.observer && token.visible && RoofsLayer.inBounds(tile, token)
+        )
         .forEach((token) => {
           token.visible = false;
         });
@@ -275,15 +283,14 @@ export default class RoofsLayer extends CanvasLayer {
   }
 
   static _patchSight() {
-    if (game.data.version.startsWith('0.6')) {
+    if (game.data.version.startsWith("0.6")) {
       const origUpdate = canvas.sight.update;
       canvas.sight.update = function update(...args) {
         origUpdate.call(this, ...args);
         RoofsLayer._sightUpdate();
       };
       canvas.sight.update();
-    }
-    else {
+    } else {
       const origUpdate = canvas.sight.refresh;
       canvas.sight.refresh = function refresh(...args) {
         origUpdate.call(this, ...args);
@@ -300,7 +307,9 @@ export default class RoofsLayer extends CanvasLayer {
     // eslint-disable-next-line camelcase
     const og_onDragLeftStart = Tile.prototype._onDragLeftStart;
     Tile.prototype._onDragLeftStart = function _onDragLeftStart(event) {
-      const targets = this.layer.options.controllableObjects ? this.layer.controlled : [this];
+      const targets = this.layer.options.controllableObjects
+        ? this.layer.controlled
+        : [this];
       RoofsLayer.hide(targets);
       og_onDragLeftStart.call(this, event);
     };
@@ -308,7 +317,9 @@ export default class RoofsLayer extends CanvasLayer {
     // eslint-disable-next-line camelcase
     const og_onDragLeftDrop = Tile.prototype._onDragLeftDrop;
     Tile.prototype._onDragLeftDrop = function _onDragLeftDrop(event) {
-      const targets = this.layer.options.controllableObjects ? this.layer.controlled : [this];
+      const targets = this.layer.options.controllableObjects
+        ? this.layer.controlled
+        : [this];
       RoofsLayer.show(targets);
       og_onDragLeftDrop.call(this, event);
     };
@@ -316,7 +327,9 @@ export default class RoofsLayer extends CanvasLayer {
     // eslint-disable-next-line camelcase
     const og_onDragLeftCancel = Tile.prototype._onDragLeftCancel;
     Tile.prototype._onDragLeftCancel = function _onDragLeftCancel(event) {
-      const targets = this.layer.options.controllableObjects ? this.layer.controlled : [this];
+      const targets = this.layer.options.controllableObjects
+        ? this.layer.controlled
+        : [this];
       RoofsLayer.show(targets);
       og_onDragLeftCancel.call(this, event);
     };
@@ -332,25 +345,30 @@ export default class RoofsLayer extends CanvasLayer {
   static async extendTileHUD(hud, html) {
     // Get props
     const tile = hud.object;
-    const isRoof = tile.getFlag('roofs', 'isRoof');
+    const isRoof = tile.getFlag("roofs", "isRoof");
     const { open, closed, mode } = RoofsLayer.getOpacity(tile);
 
     // Append template
-    const form = await renderTemplate('/modules/roofs/templates/hud.hbs', { isRoof, open, closed, mode });
-    html.find('.col.left').append(form);
+    const form = await renderTemplate("/modules/roofs/templates/hud.hbs", {
+      isRoof,
+      open,
+      closed,
+      mode,
+    });
+    html.find(".col.left").append(form);
 
     // Send to roof
-    html.find('.roof').click(async () => {
+    html.find(".roof").click(async () => {
       await RoofsLayer.receiveTile(tile);
       hud.render();
     });
     // Send to floor
-    html.find('.floor').click(async () => {
+    html.find(".floor").click(async () => {
       await RoofsLayer.releaseTile(tile);
       hud.render();
     });
     // Toggle mode
-    html.find('.mode').click(async () => {
+    html.find(".mode").click(async () => {
       await RoofsLayer.toggleMode(tile);
       hud.render();
     });
@@ -358,13 +376,13 @@ export default class RoofsLayer extends CanvasLayer {
     const openSlider = html.find('input[name="open"]');
     openSlider.change(() => {
       const val = parseFloat(openSlider[0].value);
-      tile.setFlag('roofs', 'open', val);
+      tile.setFlag("roofs", "open", val);
     });
     // closed state opacity slider
     const closedSlider = html.find('input[name="closed"]');
     closedSlider.change(() => {
       const val = parseFloat(closedSlider[0].value);
-      tile.setFlag('roofs', 'closed', val);
+      tile.setFlag("roofs", "closed", val);
     });
   }
 }
--- scripts/roofs.js
+++ scripts/roofs.js
@@ -1,30 +1,39 @@
-import RoofsLayer from './RoofsLayer.js';
-import config from './config.js';
-import { log } from './helpers.js';
+import RoofsLayer from "./RoofsLayer.js";
+import config from "./config.js";
+import { log } from "./helpers.js";
 
 /**
  * Load templates and config
  */
-Hooks.once('init', async () => {
-  log('Initializing', true);
+Hooks.once("init", async () => {
+  log("Initializing", true);
   config();
-  loadTemplates([
-    'modules/roofs/templates/hud.hbs',
-  ]);
+  loadTemplates(["modules/roofs/templates/hud.hbs"]);
 });
 
 /**
  * Add RoofsLayer to canvas
  */
-Hooks.once('canvasInit', () => {
+Hooks.once("canvasInit", () => {
   const index = canvas.stage.getChildIndex(canvas.lighting);
   canvas.roofs = canvas.stage.addChildAt(new RoofsLayer(), index);
+  const layers = canvas.layers;
+  layers.push(canvas.roofs);
+  Object.defineProperty(canvas, "layers", {
+    get: function () {
+      return layers;
+    },
+  });
+});
+
+Hooks.on("canvasReady", () => {
+  canvas.roofs.zIndex = canvas.lighting.zIndex + 1;
 });
 
 /**
  *  Apply patches to core methods
  */
-Hooks.once('ready', () => {
+Hooks.once("ready", () => {
   RoofsLayer._patchDrag();
   RoofsLayer._patchSight();
 });
@@ -32,11 +41,11 @@ Hooks.once('ready', () => {
 /**
  * Pass hooks to RoofsLayer
  */
-Hooks.on('canvasReady', RoofsLayer.init);
-Hooks.on('updateTile', RoofsLayer._onUpdateTile);
-Hooks.on('preDeleteTile', RoofsLayer._onPreDeleteTile);
-Hooks.on('renderTileHUD', RoofsLayer.extendTileHUD);
-Hooks.on('updateToken', RoofsLayer._onUpdateToken);
-Hooks.on('hoverTile', RoofsLayer._onHoverTile);
+Hooks.on("canvasReady", RoofsLayer.init);
+Hooks.on("updateTile", RoofsLayer._onUpdateTile);
+Hooks.on("preDeleteTile", RoofsLayer._onPreDeleteTile);
+Hooks.on("renderTileHUD", RoofsLayer.extendTileHUD);
+Hooks.on("updateToken", RoofsLayer._onUpdateToken);
+Hooks.on("hoverTile", RoofsLayer._onHoverTile);
 
 window.RoofsLayer = RoofsLayer;
